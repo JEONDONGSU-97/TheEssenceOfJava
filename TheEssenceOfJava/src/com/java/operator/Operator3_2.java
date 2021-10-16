a7f616d87971e84940f4df98b9ca14acb57884c7
JEONDONGSU-97 <alrnr3521@naver.com>
2021-10-16 22:10:58 +0900

    2021.10.16 자바의 정석

---
+++ TheEssenceOfJava/src/com/java/operator/Operator3_2.java
@@ -0,0 +1,18 @@
+package com.java.operator;
+
+public class Operator3_2 {
+
+	public static void main(String[] args) {
+		
+		int x = 2;
+		int y = 5;
+		char c = 'A';
+		
+		System.out.println('C' - c);	
+		System.out.println(++c);		 
+		System.out.println(c++);		
+		System.out.println(c);			
+	}
+
+}
+
---
+++ TheEssenceOfJava/src/com/java/variable/Variable2_7.java
@@ -0,0 +1,14 @@
+package com.java.variable;
+
+public class Variable2_7 {
+
+	public static void main(String[] args) {
+		System.out.println(true+ "");
+		System.out.println('A'+'B');	// 'A'와'B'의 문자코드의 값 65,66
+		System.out.println('1'+2);		// '1'의 문자코드의 값 49
+		System.out.println('1'+'2');	// '2'의 문자코드의 값 50
+		System.out.println('J'+"ava");  // 문자열과 덧셈연산을 하면 그 결과는 항상 문자열이 된다.
+	}
+
+}
+
